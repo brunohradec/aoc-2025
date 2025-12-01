004cc99bdcebd5e74626fbef2ab44fe193885c17
Bruno Hradec <bhradec@gmail.com>
2025-12-02 00:20:29 +0100

    Improve zero detector code

--- src/main/java/me/bhradec/day1/part2/ZeroDetector.java
+++ src/main/java/me/bhradec/day1/part2/ZeroDetector.java
@@ -1,13 +1,18 @@
 package me.bhradec.day1.part2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ZeroDetector {
-    private int wrapCount = 0;
+    private int zeroCount = 0;
+    private final Logger log = LoggerFactory.getLogger(ZeroDetector.class);
 
     public void handleZeroDetected() {
-        this.wrapCount++;
+        log.info("Zero pass detected");
+        this.zeroCount++;
     }
 
     public int getZeroCount() {
-        return this.wrapCount;
+        return this.zeroCount;
     }
 }
